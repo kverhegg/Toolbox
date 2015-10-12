f3b4f2bcefea090d3369bca1f7255ade61720f84
Kenneth Verheggen <kenneth.verheggen@gmail.com>
2015-10-12 14:12:01 +0200

    Added example for other objects

--- Toolbox/src/main/java/com/compomics/toolbox/json/Main.java
+++ Toolbox/src/main/java/com/compomics/toolbox/json/Main.java
@@ -6,6 +6,7 @@
 package com.compomics.toolbox.json;
 
 import com.compomics.toolbox.json.converters.SearchParameterConverter;
+import com.compomics.util.experiment.biology.Protein;
 import com.compomics.util.experiment.identification.identification_parameters.SearchParameters;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,8 +19,6 @@ import java.util.Objects;
  */
 public class Main {
 
-    private static final DefaultJsonConverter jsonConverter = new SearchParameterConverter();
-
     public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException {
         //loading the initial file
         ClassLoader classLoader = Main.class.getClassLoader();
@@ -27,14 +26,32 @@ public class Main {
         //create the original search parameters
         SearchParameters originalParameters = SearchParameters.getIdentificationParameters(parameterFile);
         //convert the parameters and save to a file
+
+        DefaultJsonConverter jsonConverter = new SearchParameterConverter();
+
         File jsonFile = new File(parameterFile.getAbsolutePath() + ".json");
+        jsonFile.deleteOnExit();
+        
         jsonConverter.saveObjectToJson(originalParameters, jsonFile);
         //convert the jsonFile back to a parameterObject
         SearchParameters convertedParameters = (SearchParameters) jsonConverter.fromJson(SearchParameters.class, jsonFile);
+
         //check if the contects still match
         System.out.println("Conversion fasta matches original:\t" + originalParameters.getFastaFile().equals(convertedParameters.getFastaFile()));
         System.out.println("Conversion enzyme matches original:\t" + originalParameters.getEnzyme().getName().equals(convertedParameters.getEnzyme().getName()));
         System.out.println("Conversion frag ion acc matches original:\t" + (Objects.equals(originalParameters.getFragmentIonAccuracy(), convertedParameters.getFragmentIonAccuracy())));
+
+        //Also works on other objects !!!!
+        Protein aProtein = new Protein("KV1988", "KENNETHISTHEMOSTAWESOMEJSONPARSERBUILDERINTHEWORLD", false);
+        DefaultJsonConverter converter = new DefaultJsonConverter();
+        String proteinAsJson = converter.toJson(aProtein);
+        Protein bProtein = (Protein) converter.fromJson(Protein.class, proteinAsJson);
+        System.out.println("Same protein : "
+                + (bProtein.getAccession().equals(aProtein.getAccession())
+                && (bProtein.getSequence().equals(aProtein.getSequence()))
+                && (bProtein.isDecoy() == aProtein.isDecoy())
+                && (bProtein.computeMolecularWeight() == aProtein.computeMolecularWeight())));
+        System.out.println(proteinAsJson);
     }
 
 }
