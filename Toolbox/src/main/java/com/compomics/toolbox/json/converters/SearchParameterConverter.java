7322f5ded0574cd29fcc318f69e2ef30180970f1
Kenneth Verheggen <kenneth.verheggen@gmail.com>
2015-10-12 14:36:13 +0200

    Update Javadoc

--- Toolbox/src/main/java/com/compomics/toolbox/json/converters/SearchParameterConverter.java
+++ Toolbox/src/main/java/com/compomics/toolbox/json/converters/SearchParameterConverter.java
@@ -7,14 +7,21 @@ package com.compomics.toolbox.json.converters;
 
 /**
  *
- * @author Kenneth
+ * @author Kenneth Verheggen
  */
 import com.compomics.toolbox.json.DefaultJsonConverter;
 import com.compomics.util.experiment.biology.Atom;
 import com.compomics.util.experiment.identification.identification_parameters.IdentificationAlgorithmParameter;
 
+/**
+ * This class is a convenience class to have a DefaultJsonConverter with the search parameter interfaces
+ * @author Kenneth Verheggen
+ */
 public class SearchParameterConverter extends DefaultJsonConverter {
 
+    /**
+     * Default constructor
+     */
     public SearchParameterConverter() {
         super(IdentificationAlgorithmParameter.class, Atom.class);
     }
